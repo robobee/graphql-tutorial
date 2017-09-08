5768926f34e84763bc786552e744b787d2c777e9
robobee <igor.serpak@gmail.com>
2017-09-08 17:48:57 +0300

    Add user to created links

--- app/graphql/types/link_type.rb
+++ app/graphql/types/link_type.rb
@@ -7,4 +7,8 @@ Types::LinkType = GraphQL::ObjectType.define do
   field :id, !types.ID
   field :url, !types.String
   field :description, !types.String
+  # add postedBy field to Link type
+  # - "-> { }": helps against loading issues between types
+  # - "property": remaps field to an attribute of Link model
+  field :postedBy, -> { Types::UserType }, property: :user
 end
